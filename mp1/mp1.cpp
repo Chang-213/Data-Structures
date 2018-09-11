eeebb82aa34bd53788a4624584c810e4d2a58f8a
Kevin Lee <changml2@linux-a1.ews.illinois.edu>
2018-09-11 17:44:14 -0500

    file submission

--- mp1/mp1.cpp
+++ mp1/mp1.cpp
@@ -1,9 +1,11 @@
+#include <cstdlib>
+#include <cmath>
 #include "cs225/PNG.h"
 using cs225::PNG;
 
 #include "cs225/HSLAPixel.h"
 using cs225::HSLAPixel;
-
+using namespace cs225;
 #include <string>
 
 
@@ -13,15 +15,15 @@ PNG* setupOutput(unsigned w, unsigned h) {
 }
 
 void rotate(std::string inputFile, std::string outputFile) {
-  // TODO: Part 2
+
   PNG* original = new PNG;
   original->readFromFile(inputFile);
-  unsigned width = original->width() const;
-  unsigned height = original->height() const;
+  unsigned width = original->width();
+  unsigned height = original->height();
   PNG* output = setupOutput(width, height);
-  for (unsigned y = 1; y < height; y++) {
-      for (unsigned x = 1; x < width; x++) {
-        HSLAPixel flip = original->getPixel(width - x, height - y);
+  for (unsigned y = 0; y < height; y++) {
+      for (unsigned x = 0; x < width; x++) {
+        HSLAPixel flip = (*original).getPixel((width-1) - x, (height-1) - y);
         output->getPixel(x,y) = flip;
       }
     }
@@ -31,7 +33,32 @@ void rotate(std::string inputFile, std::string outputFile) {
 
 PNG myArt(unsigned int width, unsigned int height) {
   PNG png(width, height);
-  // TODO: Part 3
 
+  for(unsigned y = 0; y < height; y++){
+    for(unsigned x = 0; x < width; x++){
+      if(x < width/3){
+        HSLAPixel hoop = png.getPixel(x, y);
+        hoop.h = 140;
+        hoop.l = 0.5;
+        hoop.s = 1;
+        png.getPixel(x, y) = hoop;
+      }
+      if(x > (2*width)/3){
+        HSLAPixel hoop = png.getPixel(x, y);
+        hoop.h = 10;
+        hoop.l = 0.5;
+        hoop.s = 1;
+        png.getPixel(x, y) = hoop;
+      }
+      if(x >= width/3 && x <=(2*width)/3){
+        HSLAPixel hoop = png.getPixel(x, y);
+        hoop.h = 80;
+        hoop.l = 0.5;
+        hoop.s = 1;
+        png.getPixel(x, y) = hoop;
+      }
+
+    }
+  }
   return png;
 }
