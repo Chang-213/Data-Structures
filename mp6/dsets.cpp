82afaf74509792161eac40e897fcc6122e0f8276
Kevin Lee <changml2@eceb-2022-10.ews.illinois.edu>
2018-11-12 19:34:44 -0600

    file submission

--- mp6/dsets.cpp
+++ mp6/dsets.cpp
@@ -1,1 +1,42 @@
 /* Your code here! */
+#include "dsets.h"
+
+DisjointSets::DisjointSets(){
+
+}
+
+void DisjointSets::addelements(int num){
+  for(int i = 0; i < num; i++){
+    _elem.push_back(-1);
+  }
+}
+
+int DisjointSets::find(int elem){
+  if(_elem[elem] < 0){
+    return elem;
+  }
+  else{
+    return find(_elem[elem]);
+  }
+}
+
+void DisjointSets::setunion(int a, int b){
+int roota = find(a);
+int rootb = find(b);
+int sizea = size(roota);
+int sizeb = size(rootb);
+int newsize = size(roota) + size(rootb);
+if(sizea < sizeb){
+  _elem[roota] = rootb;
+  _elem[rootb] = -1*newsize;
+}
+else{
+  _elem[rootb] = roota;
+  _elem[roota] = -1*newsize;
+}
+}
+
+int DisjointSets::size(int elem){
+int in = find(elem);
+return (-1*_elem[in]);
+}
--- mp6/dsets.h
+++ mp6/dsets.h
@@ -1,1 +1,17 @@
 /* Your code here! */
+#pragma once
+
+#include <vector>
+using namespace std;
+
+class DisjointSets
+{
+  public:
+    DisjointSets();
+    void addelements(int num);
+    int find(int elem);
+    void setunion(int a, int b);
+    int size(int elem);
+  private:
+    vector<int> _elem;
+};
